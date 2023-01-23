bae9d3e40d0dfdffac1a38c828bb848275e298e7
Iamvictorsam <iamvictorsam@gmail.com>
2023-01-23 15:15:51 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Counter from "./lib/Counter.svelte";
+
+</script>
+
+<main>
+  <div class="card">
+    <Counter></Counter>
+  </div>
+
+  
+ 
+  <h1>Vite + Svelte</h1>
+
+  <!-- !Warning: it works, don't touch -->
+  <!-- *Important: This is very important -->
+  <!-- ?Question: Why is this working -->
+  <!-- Todo: Make Add Cart Function -->
+  <!-- ! -->
+
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+
+
+<style>
+  
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+
+  .read-the-docs {
+    color: #888;
+  }
+
+</style>
+
+
